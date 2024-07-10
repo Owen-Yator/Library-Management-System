06a1ce5e068656ae4cc64f27b4fcad4c1545bd3a
Blur-gene <yatorowen@gmail.com>
2024-07-10 17:54:34 +0300

    Add project files.

---
+++ Library Management System/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        txtTitle = New TextBox()
+        txtAuthor = New TextBox()
+        txtYearPublished = New TextBox()
+        txtGenre = New TextBox()
+        btnAdd = New Button()
+        btnUpdate = New Button()
+        btnDelete = New Button()
+        btnClear = New Button()
+        dgvBooks = New DataGridView()
+        CType(dgvBooks, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(30, 19)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(40, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Title"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(30, 53)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(59, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Author"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(30, 89)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(111, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Year Published"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(30, 128)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(51, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "Genre"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(469, 161)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(0, 15)
+        Label5.TabIndex = 4
+        ' 
+        ' txtTitle
+        ' 
+        txtTitle.Location = New Point(153, 11)
+        txtTitle.Name = "txtTitle"
+        txtTitle.Size = New Size(111, 23)
+        txtTitle.TabIndex = 5
+        ' 
+        ' txtAuthor
+        ' 
+        txtAuthor.Location = New Point(153, 45)
+        txtAuthor.Name = "txtAuthor"
+        txtAuthor.Size = New Size(111, 23)
+        txtAuthor.TabIndex = 6
+        ' 
+        ' txtYearPublished
+        ' 
+        txtYearPublished.Location = New Point(153, 81)
+        txtYearPublished.Name = "txtYearPublished"
+        txtYearPublished.Size = New Size(111, 23)
+        txtYearPublished.TabIndex = 7
+        ' 
+        ' txtGenre
+        ' 
+        txtGenre.Location = New Point(153, 120)
+        txtGenre.Name = "txtGenre"
+        txtGenre.Size = New Size(111, 23)
+        txtGenre.TabIndex = 8
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Font = New Font("Lucida Sans", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnAdd.Location = New Point(30, 183)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(75, 23)
+        btnAdd.TabIndex = 9
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.Font = New Font("Lucida Fax", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnUpdate.Location = New Point(153, 183)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(86, 23)
+        btnUpdate.TabIndex = 10
+        btnUpdate.Text = "Update"
+        btnUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Font = New Font("Lucida Fax", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnDelete.Location = New Point(30, 234)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(75, 23)
+        btnDelete.TabIndex = 11
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Font = New Font("Lucida Fax", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnClear.Location = New Point(153, 234)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(86, 23)
+        btnClear.TabIndex = 12
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' dgvBooks
+        ' 
+        dgvBooks.BackgroundColor = SystemColors.ButtonHighlight
+        dgvBooks.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvBooks.Location = New Point(287, 2)
+        dgvBooks.Name = "dgvBooks"
+        dgvBooks.Size = New Size(491, 174)
+        dgvBooks.TabIndex = 13
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(800, 450)
+        Controls.Add(dgvBooks)
+        Controls.Add(btnClear)
+        Controls.Add(btnDelete)
+        Controls.Add(btnUpdate)
+        Controls.Add(btnAdd)
+        Controls.Add(txtGenre)
+        Controls.Add(txtYearPublished)
+        Controls.Add(txtAuthor)
+        Controls.Add(txtTitle)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(dgvBooks, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtAuthor As TextBox
+    Friend WithEvents txtYearPublished As TextBox
+    Friend WithEvents txtGenre As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents dgvBooks As DataGridView
+
+End Class
---
+++ Library Management System/Form1.vb
@@ -0,0 +1,154 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    'variables used to connect to the DB
+    Dim sqlconn As SqlConnection
+    Dim sqlcmd As SqlCommand
+    Dim sqlRd As SqlDataReader
+    Dim sqlDt As New DataTable
+    Dim DtA As New SqlDataAdapter
+    Dim sqlQuery As String
+
+    'The my mssql server and database name
+    Dim Server As String = "DESKTOP-FLHSDEK\MSSQLSERVER1"
+    Dim Database As String = "LibraryDB"
+
+    Private bitmap As Bitmap
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        updatetable()
+    End Sub
+
+    Private Sub updatetable()
+        Try
+            sqlconn = New SqlConnection()
+            sqlconn.ConnectionString = $"Server={Server};Database={Database};Integrated Security=True;"
+
+            sqlconn.Open()
+            sqlcmd = New SqlCommand("SELECT * FROM Books", sqlconn)
+            sqlRd = sqlcmd.ExecuteReader()
+
+            sqlDt.Clear()
+            sqlDt.Load(sqlRd)
+            sqlRd.Close()
+            sqlconn.Close()
+            dgvBooks.DataSource = sqlDt
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "MSSQL Connector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If sqlconn IsNot Nothing AndAlso sqlconn.State = ConnectionState.Open Then
+                sqlconn.Close()
+            End If
+        End Try
+    End Sub
+
+    'Functionality of the ADD button
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Try
+            sqlconn = New SqlConnection()
+            sqlconn.ConnectionString = $"Server={Server};Database={Database};Integrated Security=True;"
+
+            sqlconn.Open()
+
+            sqlQuery = "INSERT INTO Books (Title, Author, YearPublished, Genre) VALUES (@Title, @Author, @YearPublished, @Genre)"
+
+            sqlcmd = New SqlCommand(sqlQuery, sqlconn)
+            sqlcmd.Parameters.AddWithValue("@Title", txtTitle.Text)
+            sqlcmd.Parameters.AddWithValue("@Author", txtAuthor.Text)
+            sqlcmd.Parameters.AddWithValue("@YearPublished", txtYearPublished.Text)
+            sqlcmd.Parameters.AddWithValue("@Genre", txtGenre.Text)
+            sqlcmd.ExecuteNonQuery()
+            sqlconn.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "MSSQL Connector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If sqlconn IsNot Nothing AndAlso sqlconn.State = ConnectionState.Open Then
+                sqlconn.Close()
+            End If
+        End Try
+
+        updatetable()
+    End Sub
+
+
+
+    ' Functionality of the UPDATE button
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Try
+            sqlconn = New SqlConnection()
+            sqlconn.ConnectionString = $"Server={Server};Database={Database};Integrated Security=True;"
+
+            sqlconn.Open()
+            'updates the row according to its title
+            sqlQuery = "UPDATE Books SET Author = @Author, Genre = @Genre, YearPublished = @YearPublished WHERE Title = @Title"
+
+            sqlcmd = New SqlCommand(sqlQuery, sqlconn)
+            sqlcmd.Parameters.AddWithValue("@Author", txtAuthor.Text)
+            sqlcmd.Parameters.AddWithValue("@Genre", txtGenre.Text)
+            sqlcmd.Parameters.AddWithValue("@YearPublished", Integer.Parse(txtYearPublished.Text))
+            sqlcmd.Parameters.AddWithValue("@Title", txtTitle.Text)
+            sqlcmd.ExecuteNonQuery()
+            sqlconn.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "MSSQL Connector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If sqlconn IsNot Nothing AndAlso sqlconn.State = ConnectionState.Open Then
+                sqlconn.Close()
+            End If
+        End Try
+
+        updatetable()
+    End Sub
+    'DELETE button functionality
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Try
+            sqlconn = New SqlConnection()
+            sqlconn.ConnectionString = $"Server={Server};Database={Database};Integrated Security=True;"
+
+            sqlconn.Open()
+            'deletes the row wher a certain title exists
+            sqlQuery = "DELETE FROM Books WHERE Title = @Title"
+
+            sqlcmd = New SqlCommand(sqlQuery, sqlconn)
+            sqlcmd.Parameters.AddWithValue("@Author", txtAuthor.Text)
+            sqlcmd.Parameters.AddWithValue("@Genre", txtGenre.Text)
+            sqlcmd.Parameters.AddWithValue("@YearPublished", Integer.Parse(txtYearPublished.Text))
+            sqlcmd.Parameters.AddWithValue("@Title", txtTitle.Text)
+            sqlcmd.ExecuteNonQuery()
+            sqlconn.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "MSSQL Connector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If sqlconn IsNot Nothing AndAlso sqlconn.State = ConnectionState.Open Then
+                sqlconn.Close()
+            End If
+        End Try
+
+        updatetable()
+    End Sub
+
+    'CLEAR button functionality
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Try
+            sqlconn = New SqlConnection()
+            sqlconn.ConnectionString = $"Server={Server};Database={Database};Integrated Security=True;"
+
+            sqlconn.Open()
+            'deletes all from table books
+            sqlQuery = "DELETE FROM Books"
+
+            sqlcmd = New SqlCommand(sqlQuery, sqlconn)
+            sqlcmd.ExecuteNonQuery()
+            sqlconn.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "MSSQL Connector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If sqlconn IsNot Nothing AndAlso sqlconn.State = ConnectionState.Open Then
+                sqlconn.Close()
+            End If
+        End Try
+
+        updatetable()
+    End Sub
+
+End Class
